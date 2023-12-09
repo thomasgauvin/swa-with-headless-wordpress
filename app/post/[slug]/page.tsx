ec36f733bd9ba2f3e340077c2d90dfd8fe6bba0f
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2023-12-09 09:24:19 -0500

    Added generate static paths

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -3,6 +3,15 @@ import { Post } from '@/app/lib/types';
 
 const wordpressUrl = process.env.WORDPRESS_URL;
 
+export async function generateStaticParams(){
+  const response = await fetch(`${wordpressUrl}/wp-json/wp/v2/posts?_fields[]=slug`);
+  const posts : Post[] = await response.json();
+  const paths = posts.map(post => ({
+    slug: post.slug 
+  }));
+  return paths;
+}
+
 export default async function BlogPage({ params }: { params: { slug: string } }) {  
     // Fetch the blog post data based on the slug (you may use an API or other method)
     const response = await fetch(`${wordpressUrl}/wp-json/wp/v2/posts/?slug=${params.slug}&_embed`);
@@ -10,7 +19,8 @@ export default async function BlogPage({ params }: { params: { slug: string } })
 
     return (
       <div>
-        <BlogPost post={post} />
+        <BlogPost post={post} /> 
       </div>
+      
     );
 };
